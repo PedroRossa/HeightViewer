1be2a8ced493dc1e3f0fe0d301a79dc4b62ad7ba
Pedro Rossa <PROSSA@asav.brm>
2019-10-20 22:26:00 -0300

    Reset leapRig. create prototype of armor menu

---
+++ Assets/Scripts/TooltipElement.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TooltipElement : MonoBehaviour
+{
+    private TextMeshPro txtMessage;
+    public string message;
+
+    void Awake()
+    {
+        txtMessage = GetComponentInChildren<TextMeshPro>();
+        txtMessage.SetText(message);
+    }
+
+    public void SetText(string text)
+    {
+        txtMessage.SetText(text);
+    }
+
+    public void ShowTooltip()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HideTooltip()
+    {
+        gameObject.SetActive(false);
+    }
+}
